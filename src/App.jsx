31c360d1b0ebdd74c54814d7b53e5ca7a01f8700
MP-William <144322860+MP-William@users.noreply.github.com>
2024-02-03 14:20:32 +0100

    i created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { landing } from './assets'
+import { DemoPage } from './pages/DemoPage'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className=" h-[1000px] w-full">
-      <img src={landing} alt="" />
-    </div>
+  <Routes>
+      <Route path="/demo" element={<DemoPage />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/DemoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Demo from '../components/Demo'
+
+export const DemoPage = () => {
+  return (
+    <div>
+
+        <Demo/>
+        
+    </div>
+  )
+}
